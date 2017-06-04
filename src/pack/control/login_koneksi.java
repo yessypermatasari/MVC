d9557261798d2db0109b0f01854132d173a6820f
Yessy Permatasari <yessypermatasari08@gmail.com>
2017-06-04 12:56:27 +0700

    MVC

---
+++ src/pack/control/login_koneksi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack.control;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Yessy Permatasari
+ */
+public class login_koneksi {
+    private static Connection koneksi;
+    public static Connection getConnection() throws SQLException
+    {
+        if (koneksi == null)
+        {
+            Driver driver = new Driver();
+            koneksi = (Connection)
+         DriverManager.getConnection("jdbc:mysql://localhost/toko?zeroDateTimeBehavior=convertToNull", "root", "");
+        }
+        return koneksi;
+    }
+}
